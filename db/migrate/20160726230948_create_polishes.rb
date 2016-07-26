11cc8260ea8ade4630a65a8be30ea2ba741d7d68
karaAJC <KaraAJC@gmail.com>
2016-07-26 18:24:14 -0500

    updating database with new migrations

--- db/migrate/20160726230948_create_polishes.rb
+++ db/migrate/20160726230948_create_polishes.rb
@@ -1,7 +1,10 @@
 class CreatePolishes < ActiveRecord::Migration
   def change
     create_table :polishes do |t|
-
+      t.string :name
+      t.string :brand
+      t.string :hex_color
+      t.string :photo_url
       t.timestamps null: false
     end
   end
